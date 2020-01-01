67c46927275636acd7c581c4bc4a976674e8ef95
test <test>
2020-01-01 16:02:20 +0800

    url cmake 重新整理

--- src/base/auto_gen/partition_alloc_buildflags.cmake
+++ src/base/auto_gen/partition_alloc_buildflags.cmake
@@ -1,4 +1,4 @@
 
 set(partition_alloc_buildflags ${CMAKE_CURRENT_SOURCE_DIR}/partition_alloc_buildflags.h)
 file(WRITE ${partition_alloc_buildflags} "#pragma once\n")
-file(APPEND ${partition_alloc_buildflags} "#define USE_PARTITION_ALLOC 0\n")
+file(APPEND ${partition_alloc_buildflags} "#define USE_PARTITION_ALLOC 1\n")
